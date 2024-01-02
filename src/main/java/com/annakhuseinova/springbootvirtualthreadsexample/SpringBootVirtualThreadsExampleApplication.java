086da39970302d252cdb554b4a8356f37f771e0a
Anna Khuseinova <annettekhuseinova@gmail.com>
2024-01-02 14:46:41 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springbootvirtualthreadsexample/SpringBootVirtualThreadsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springbootvirtualthreadsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVirtualThreadsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVirtualThreadsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/springbootvirtualthreadsexample/SpringBootVirtualThreadsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springbootvirtualthreadsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVirtualThreadsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
